d8f286f777d63961037432d80762a2085953fbfc
Samuel Gemberling <sgemb@samuels-mbp.attlocal.net>
2020-05-07 10:19:20 -0500

    Initialize repo, update readme.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# slot-machine-game
+
+## Getting Started
+[Click Here to open game in browser](www.github.com/salmon117/slot-machine.git)
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+
+## Screenshots
+
+![]()
+
+![]()
+
+## Future Enhancements
+
+1. Add login/logout.
+2. Add mobile responsiveness
---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Pseudocode
+//Modul pops up asking for how much money the person wants to input.
+//Changes that money into tokens for game.
+//Have a display that shows chances for certain icons and payout amounts for matches.
+//Person can toggle how many tokens they want to bet at a time.
+//Once chosen they pull the lever to start the spinning.(Lever resets after being clicked).
+//While spinning is happening stars light up.
+//(Hopefully) Each roller will roll through 3 random icons before landing on the icon that will be chosen.
+//After the 3 rollers are done choosing check for matches, doll out tokens accordingly.
+//Display amount of tokens won if they match 3 of a certain icon.
+//Player does not have to pick amount of tokens to bet but can choose to change amount.
+
+
+
+//Constants
+const icons = {
+    [1]: seven,
+    [2,3,4,5]: bar,
+    [6]: bell,
+    [16]: cherry,
+    [31]: grape,
+    [51]: orange,
+    [76]: lemon
+}
+//Variables
+
+//Cached DOM Elements
+
+//Event Listeners
+
+//Functions
