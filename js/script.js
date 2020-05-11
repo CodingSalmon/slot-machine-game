21b2cc806419dfa8bc30f88217df642313bab30f
Samuel Gemberling <sgemb@samuels-mbp.attlocal.net>
2020-05-10 22:19:03 -0500

    add font, make chances better

--- js/script.js
+++ js/script.js
@@ -70,55 +70,55 @@ function handlePlayClick(){
 
 function rollRoller1(){
     num = getRandomInt(100);
-    if (num === 1)
-        roller1 = 'seven';
-    if (num >= 2 && num <= 5)
+    if (num >= 1 && num <= 2)
+        roller1 = 'diamond';
+    if (num >= 3 && num <= 10)
         roller1 = 'bar';
-    if (num >= 6 && num <= 15)
+    if (num >= 11 && num <= 22)
         roller1 = 'bell';
-    if (num >= 16 && num <= 30)
+    if (num >= 23 && num <= 40)
         roller1 = 'cherry';
-    if (num >= 31 && num <= 50)
+    if (num >= 41 && num <= 55)
         roller1 = 'horseshoe';
-    if (num >= 51 && num <= 70)
+    if (num >= 56 && num <= 75)
         roller1 = 'grape';
-    if (num >= 71 && num <= 100)
+    if (num >= 76 && num <= 100)
         roller1 = 'lemon';
 };
 
 function rollRoller2(){
     num = getRandomInt(100);
-    if (num === 1)
-        roller2 = 'seven';
-    if (num >= 2 && num <= 5)
+    if (num >= 1 && num <= 2)
+        roller2 = 'diamond';
+    if (num >= 3 && num <= 10)
         roller2 = 'bar';
-    if (num >= 6 && num <= 15)
+    if (num >= 11 && num <= 22)
         roller2 = 'bell';
-    if (num >= 16 && num <= 30)
+    if (num >= 23 && num <= 40)
         roller2 = 'cherry';
-    if (num >= 31 && num <= 50)
+    if (num >= 41 && num <= 55)
         roller2 = 'horseshoe';
-    if (num >= 51 && num <= 70)
+    if (num >= 56 && num <= 75)
         roller2 = 'grape';
-    if (num >= 71 && num <= 100)
+    if (num >= 76 && num <= 100)
         roller2 = 'lemon';
 };
 
 function rollRoller3(){
     num = getRandomInt(100);
-    if (num === 1)
-        roller3 = 'seven';
-    if (num >= 2 && num <= 5)
+    if (num >= 1 && num <= 2)
+        roller3 = 'diamond';
+    if (num >= 3 && num <= 10)
         roller3 = 'bar';
-    if (num >= 6 && num <= 15)
+    if (num >= 11 && num <= 22)
         roller3 = 'bell';
-    if (num >= 16 && num <= 30)
+    if (num >= 23 && num <= 40)
         roller3 = 'cherry';
-    if (num >= 31 && num <= 50)
+    if (num >= 41 && num <= 55)
         roller3 = 'horseshoe';
-    if (num >= 51 && num <= 70)
+    if (num >= 56 && num <= 75)
         roller3 = 'grape';
-    if (num >= 71 && num <= 100)
+    if (num >= 76 && num <= 100)
         roller3 = 'lemon';
 };
 
